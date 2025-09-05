aba40835aca20812b7b1e252992f7db320ecfedd
student02 <dgdhgfjkl@rdgfh.com>
2025-09-05 08:13:08 +0200

    comiit

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,17 +24,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Switch switchLight = findViewById(R.id.lightSwitch);
-        Switch switchRollerBlind = findViewById(R.id.rollerBlindSwith);
-        Switch switchDoor = findViewById(R.id.doorSwitch);
+        ToggleButton switchLight = findViewById(R.id.lightSwitch);
+        ToggleButton switchRollerBlind = findViewById(R.id.rollerBlindSwith);
+        ToggleButton switchDoor = findViewById(R.id.doorSwitch);
         TextView statusLight = findViewById(R.id.lightStatus);
         TextView statusRollerBlind = findViewById(R.id.rollerBlindStatus);
         TextView statusDoor = findViewById(R.id.doorStatus);
 
-        if(switchLight.isChecked()){
-            statusLight.setText("ON");
-        }else{
-            statusLight.setText("OFF");
-        }
+
+        switchLight.setOnCheckedChangeListener(((buttonView, isChecked) -> {
+            if(switchLight.isChecked()){
+                statusLight.setText("ON");
+            }else{
+                statusLight.setText("OFF");
+            }
+        }));
+        switchRollerBlind.setOnCheckedChangeListener(((buttonView, isChecked) -> {
+            if(switchRollerBlind.isChecked()){
+                statusRollerBlind.setText("ON");
+            }else{
+                statusRollerBlind.setText("OFF");
+            }
+        }));
+        switchDoor.setOnCheckedChangeListener(((buttonView, isChecked) -> {
+            if(switchDoor.isChecked()){
+                statusDoor.setText("ON");
+            }else{
+                statusDoor.setText("OFF");
+            }
+        }));
+
     }
 }
